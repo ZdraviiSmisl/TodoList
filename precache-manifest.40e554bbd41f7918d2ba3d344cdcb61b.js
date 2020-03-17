839a716e13d7cd9787f28c257a33f2bab9db4286
Evgeniy <gormoshka123@gmail.com>
2020-03-18 00:36:31 +0300

    Updates

---
+++ precache-manifest.40e554bbd41f7918d2ba3d344cdcb61b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766fd53d44451c4b944b03f75c353c57",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "002fdac66cfd4411bfb7",
+    "url": "/TodoList/static/css/main.dfc64969.chunk.css"
+  },
+  {
+    "revision": "edd28ecf1a8760bc18d7",
+    "url": "/TodoList/static/js/2.5e026de2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/TodoList/static/js/2.5e026de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002fdac66cfd4411bfb7",
+    "url": "/TodoList/static/js/main.c6a303a6.chunk.js"
+  },
+  {
+    "revision": "588c4c424406449bcff3",
+    "url": "/TodoList/static/js/runtime-main.2f48f307.js"
+  }
+]);
